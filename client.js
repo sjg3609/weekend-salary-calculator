5ca96ffa8465d3e428875cfd946dbe35fef93eb5
Sam Gossie <sjg3609@gmail.com>
2023-02-17 09:55:17 -0600

    finished submitForm function and added removeRow function to delete entries

--- client.js
+++ client.js
@@ -1,5 +1,27 @@
 console.log('Hello!');
 
 function submitInfo(event) {
-    
+    console.log('submitting information');
+    event.preventDefault();
+    let firstName = document.querySelector('#firstname-input').value;
+    let lastName = document.querySelector('#lastname-input').value;
+    let id = document.querySelector('#id-input').value;
+    let title = document.querySelector('#title-input').value;
+    let salary = document.querySelector('#salary-input').value;
+    let calculatorDiv = document.querySelector('#employeesData');
+    calculatorDiv.innerHTML += `
+      <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${Number(id)}</td>
+            <td>${title}</td>
+            <td>${Number(salary)}</td>
+            <td><button onClick="removeRow(event)">Delete</button></td>
+        </tr>
+
+    `;
+}
+
+function removeRow(event) {
+    event.target.closest('tr').remove();
 }
