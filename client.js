8273581307f445d600ed7219219952ae48e8871d
Sam Gossie <sjg3609@gmail.com>
2023-02-18 15:59:56 -0600

    updated submitInfo function to change the background color of Total Monthly Salary to red

--- client.js
+++ client.js
@@ -3,12 +3,17 @@ console.log('Hello!');
 function submitInfo(event) {
     console.log('submitting information');
     event.preventDefault();
+
+    // let totalMonthlyCost += ${salary} % 12; 
+
     let firstName = document.querySelector('#firstname-input').value;
     let lastName = document.querySelector('#lastname-input').value;
     let id = document.querySelector('#id-input').value;
     let title = document.querySelector('#title-input').value;
     let salary = document.querySelector('#salary-input').value;
     let calculatorDiv = document.querySelector('#employeesData');
+    let monthlySalaryDiv = document.querySelector('#totalMonthlySalary');
+    
     calculatorDiv.innerHTML += `
       <tr>
             <td>${firstName}</td>
@@ -20,17 +25,17 @@ function submitInfo(event) {
         </tr>
 
     `;
-    
+     if (Number(salary) >= 20000) {
+        monthlySalaryDiv.style.backgroundColor = 'red';
+
+     } else {
+        monthlySalaryDiv.style.backgroundColor = '';
+     }
 }
 
 function removeRow(event) {
     event.target.closest('tr').remove();
 }
 
-function totalMonthlySalary(event) {
-    let sum = 0;
-    let totalSalary = sum;
-    return totalSalary;
-}
 
-console.log(totalMonthlySalary);
+
